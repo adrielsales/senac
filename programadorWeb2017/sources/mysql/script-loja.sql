d83d9736ec79c5d17dad90d79aefc3abb789180a
adrielsales <adrielsalles@gmail.com>
2017-09-29 18:27:09 -0300

    adicionando script teste banco loja

---
+++ programadorWeb2017/sources/mysql/script-loja.sql
@@ -0,0 +1,11 @@
+use loja_teste_senac;
+
+insert into produto (nome, quantidade) 
+values
+("Monitor", 10),
+("Pendrive", 50),
+("Mochila Dell", 25),
+("Windows 10 Home", 100),
+("Mouse sem Fio", 35);
+
+
