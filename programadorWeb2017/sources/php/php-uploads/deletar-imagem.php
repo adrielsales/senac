371a790fc038e7e5a4f9b79849df0a77a0a0fecb
adrielsales <adrielsalles@gmail.com>
2017-11-03 19:07:42 -0300

    last code 2

--- programadorWeb2017/sources/php/php-uploads/deletar-imagem.php
+++ programadorWeb2017/sources/php/php-uploads/deletar-imagem.php
@@ -3,10 +3,15 @@ require_once 'conexao.php';
 
 $id = (int) $_GET['id'];
 
+$sql = "SELECT * FROM imagens WHERE id = $id LIMIT 1";
+$resultado = mysqli_query($conexao, $sql);
+$imagem = mysqli_fetch_object($resultado);
+
 $sql = "DELETE FROM imagens WHERE id = $id";
 $resultado = mysqli_query($conexao, $sql);
 
 if($resultado){
-  header("Location:listar-imagens.php?mensagem=Excluído+com+Sucesso!");
-  d1ie();
+	unlink("uploads/".$imagem->nome);
+  	header("Location:listar-imagens.php?mensagem=Excluído+com+Sucesso!");
+  	die();
 }
